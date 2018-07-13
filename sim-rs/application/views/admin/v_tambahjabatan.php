a6c5f82979be2d9673a818ea397623d1fc049002
AlyaWulandari <34365384+AlyaWulandari@users.noreply.github.com>
2018-07-13 13:24:05 +0800

    Add files via upload

---
+++ sim-rs/application/views/admin/v_tambahjabatan.php
@@ -0,0 +1,46 @@
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Master Data
+      </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- Default box -->
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Tambah Data Jabatan</h3>
+        </div>
+        <div class="box-body">
+            <!-- form start -->
+            <form class="form-horizontal" action="<?php echo base_url('Admin/c_masterdata/input_jabatan') ?>" method="post">
+              <div class="box-body">
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">Jabatan</label>
+                  <div class="col-sm-10">
+                    <input type="text" name="jabatan" class="form-control" placeholder="Jabatan" >
+                  </div>
+                </div>
+              </div>
+              <!-- /.box-body -->
+              <div class="timeline-footer">
+                <div class="" align="right">
+                  <div class="col-sm-12"> 
+                    <a type="button" class="btn btn-danger" href="<?php echo base_url()."Admin/c_masterdata/data_jabatan"?>">Batal</a>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+ 

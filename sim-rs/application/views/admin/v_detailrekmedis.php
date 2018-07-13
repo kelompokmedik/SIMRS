acc2b71a706ddcf8c8cc593d771827bc2facbd4a
asriandyramli <34365581+asriandyramli@users.noreply.github.com>
2018-07-13 13:12:14 +0800

    Add files via upload

---
+++ sim-rs/application/views/admin/v_detailrekmedis.php
@@ -0,0 +1,236 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Riwayat
+        <small>Medis</small>
+      </h1>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="page-header">DATA REKAM MEDIK</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="form-group row">
+                    <label  class="control-label col-md-1 col-sm-1 col-xs-12">Nama</label>
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                      <input class="form-control col-md-8 col-xs-12" type="text" name="nama">
+                    </div>
+                    <label  class="control-label col-md-1 col-sm-1 col-xs-12">J.Kelamin</label>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                      <input class="form-control col-md-2 col-xs-12" type="text" name="jk">
+                    </div>
+              </div>
+              <div class="form-group row">
+                    <label  class="control-label col-md-1 col-sm-1 col-xs-12">Alamat</label>
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                      <input class="form-control col-md-8 col-xs-12" type="text" name="alamat">
+                    </div>
+                    <label  class="control-label col-md-1 col-sm-1 col-xs-12">No Telp</label>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                      <input class="form-control col-md-7 col-xs-12" type="text" name="telp">
+                    </div>
+              </div>
+               <div class="form-group row">
+                    <label  class="control-label col-md-1 col-sm-1 col-xs-12">Tmp. lahir</label>
+                    <div class="col-md-2 col-sm-4 col-xs-12">
+                      <input class="form-control col-md-8 col-xs-12" type="text" name="t_lahir">
+                    </div>
+                    <label  class="control-label col-md-2 col-sm-2 col-xs-12">Usia</label>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                      <input class="form-control col-md-7 col-xs-12" type="text" name="hobi">
+                    </div>
+                     <label  class="control-label col-md-1 col-sm-1 col-xs-12">Status</label>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                      <input class="form-control col-md-7 col-xs-12" type="text" name="hobi">
+                    </div>
+              </div>
+               <div class="form-group row">
+                    <label  class="control-label col-md-1 col-sm-1 col-xs-12">Tgl. lahir</label>
+                    <div class="col-md-2 col-sm-4 col-xs-12">
+                      <input class="form-control col-md-8 col-xs-12" type="number" name="telpon">
+                    </div>
+                    <label  class="control-label col-md-2 col-sm-2 col-xs-12">Agama</label>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                      <input class="form-control col-md-7 col-xs-12" type="text" name="hobi">
+                    </div>
+                     <label  class="control-label col-md-1 col-sm-1 col-xs-12">Pekerjaan</label>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                      <input class="form-control col-md-7 col-xs-12" type="text" name="hobi">
+                    </div>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+
+          <div class="box">
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="accordion" id="accordionExample">
+                <div class="card">
+                  <div class="card-header" id="headingOne">
+                    <h5 class="mb-0">
+                      <button class="btn btn-primary btn-block " type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <div align="left" style="color: white;">Detail Biodata Pasien</div>
+                      </button>
+                    </h5>
+                  </div>
+
+                  <div id="collapseOne" class="collapse " aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-header" id="headingTwo">
+                    <h5 class="mb-0">
+                      <button class="btn btn-primary btn-block collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <div align="left" style="color: white;">Rawat Inap</div>
+                      </button>
+                    </h5>
+                  </div>
+                  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                    <div class="card-body">
+                          <!-- /.box-header -->
+                          <div class="container">
+                          <div class="table-responsive ">
+                            <table  class="table table-bordered table-striped" id="example1">
+                              <thead>
+                              <tr>
+                                <th>Tanggal</th>
+                                <th>Ruangan</th>
+                                <th>Kelas</th>
+                                <th>No Kamar</th>
+                                <th>Dokter</th>
+                                <th>Diagnosa</th>
+                                <th>Biaya</th>
+                                <th>Detail</th>
+                              </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td></td>
+                                  <td></a></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>  
+                                </tr>
+                              </tbody>
+                            </table>
+                       </div>
+                       </div>
+                     </div>
+                   </div>
+                </div>
+                <article>
+
+                <div class="card">
+                  <div class="card-header" id="headingThree">
+                    <h5 class="mb-0">
+                      <button class="btn btn-primary  btn-block collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <div align="left" style="color: white;">Rawat Jalan</div>
+                      </button>
+                    </h5>
+                  </div>
+                  <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div class="card-body">
+                          <!-- /.box-header -->
+                          <div class="box-body">
+                            <table id="example1" class="table table-bordered table-striped">
+                              <thead>
+                              <tr>
+                                <th>Tanggal</th>
+                                <th>Ruangan</th>
+                                <th>Kelas</th>
+                                <th>No Kamar</th>
+                                <th>Dokter</th>
+                                <th>Diagnosa</th>
+                                <th>Biaya</th>
+                                <th>Detail</th>
+                              </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td></td>
+                                  <td></a></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                  <td></td>
+                                </tr>
+                              </tbody>
+                            </table>
+                          </div>
+                    </div>
+                  </div>
+
+                <div class="card">
+                  <div class="card-header" id="headingThree">
+                    <h5 class="mb-0">
+                      <button class="btn btn-primary  btn-block collapsed" type="button" data-toggle="collapse" data-target="#collapseThree3" aria-expanded="false" aria-controls="collapseThree">
+                        <div align="left" style="color: white;"> Rawat UGD</div>
+                      </button>
+                    </h5>
+                  </div>
+                  <div id="collapseThree3" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div class="card-body">
+                      <section class="content">
+                        <div class="row">
+                          <div class="col-xs-12">
+                            <div class="box">
+                              <!-- /.box-header -->
+                              <div class="box-body">
+                                <table id="example1" class="table table-bordered table-striped">
+                                  <thead>
+                                  <tr>
+                                    <th>No</th>
+                                    <th>Nama Pegawai</th>
+                                    <th>Jabatan</th>
+                                    <th>Pangkat</th>
+                                    <th>Action</th>
+                                  </tr>
+                                  </thead>
+                                  <tbody>
+                                  </tbody>
+                                </table>
+                                <div class="timeline-footer">
+                                </div>
+                              </div>
+                              <!-- /.box-body -->
+                            </div>
+                            <!-- /.box -->
+                          </div>
+                          <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                      </section>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->

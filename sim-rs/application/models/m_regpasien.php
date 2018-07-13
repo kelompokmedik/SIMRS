63f7dbd425062e7f0a44f25f2cf4a0b14b3c4fb5
Alfhiraa <40827985+Alfhiraa@users.noreply.github.com>
2018-07-13 12:38:11 +0800

    Add files via upload

---
+++ sim-rs/application/models/m_regpasien.php
@@ -0,0 +1,24 @@
+<?php
+	class M_regpasien extends CI_Model{
+
+		function tampil_data($table){
+			return $this->db->get($table)->result();
+		}
+		function input_data($data,$tabel){
+			$ber = $this->db->insert($tabel,$data);
+			return $ber;
+		}
+		function edit_data($where,$tabel){
+			return $this->db->get_where($tabel,$where);
+		}
+		function update_data($where,$data,$table){
+			$this->db->where($where);
+			$this->db->update($table,$data);
+		}
+		function hapus_data($where,$tabel){
+			$this->db->where($where);
+			$this->db->delete($tabel);
+		}
+
+	}
+?>
